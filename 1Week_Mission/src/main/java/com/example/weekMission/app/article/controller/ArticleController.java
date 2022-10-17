ecf4d3c7de9b1b5163054ee613a611a43e890524
jeonga <jjeong0106@gmail.com>
2022-10-17 19:03:31 +0900

    작성버튼 클릭 시 post방식으로 요청 후 메인화면으로 돌아오도록 write.html 수정

--- 1Week_Mission/src/main/java/com/example/weekMission/app/article/controller/ArticleController.java
+++ 1Week_Mission/src/main/java/com/example/weekMission/app/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.weekMission.app.article.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class ArticleController {
     @GetMapping("/write")
     public String showWrite() {
         return "article/write";
+    }
+
+    @PostMapping("/write")
+    public String write() {
+        return "redirect:/";
     }
 }
