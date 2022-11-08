5b698436a9b5b40c2588d3905ca71664a664d128
jeonga <jjeong0106@gmail.com>
2022-11-09 02:11:02 +0900

    4week init

---
+++ 4Week_Mission/src/main/java/com/ll/exam/eBook/app/order/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.ll.exam.eBook.app.order.repository;
+
+import com.ll.exam.eBook.app.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByBuyerIdOrderByIdDesc(long buyerId);
+}
