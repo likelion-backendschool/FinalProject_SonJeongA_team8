c21c671628687e52b7802549b1c4e2eec3cb8f18
jeonga <jjeong0106@gmail.com>
2022-11-09 02:30:07 +0900

    [test] 시크릿 키 원문으로 HMAC 암호화 방식에 맞는 SecretKey 객체를 만들수 있는지 체크하는 테스트케이스 추가

--- 4Week_Mission/src/test/java/com/ll/exam/eBook/jwt/JwtTests.java
+++ 4Week_Mission/src/test/java/com/ll/exam/eBook/jwt/JwtTests.java
@@ -1,10 +1,13 @@
 package com.ll.exam.eBook.jwt;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,4 +22,12 @@ class JwtTests {
         assertThat(secretKeyPlain).isNotNull();
     }
 
+    @Test
+    @DisplayName("sercretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
+    void t2() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+
+        assertThat(secretKey).isNotNull();
+    }
 }
